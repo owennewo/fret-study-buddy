d8471f933f9588cf3aa4413b1805078d31936435
Owen Williams <williams.owen@gmail.com>
2024-06-22 22:13:20 +0100

    fixing build to make certs a local thinG

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,26 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import fs from 'fs'
 import { fileURLToPath, URL } from 'node:url'
-// import staticFiles from './vite-plugin-static-files'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    // staticFiles()
-  ],
-  base: '/fret-study-buddy/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '/artyom.window.min.js': fileURLToPath(new URL('./node_modules/artyom.js/build/artyom.window.min.js', import.meta.url))
-    }
-  },
-  server: {
-    https: {
-      key: fs.readFileSync('./localhost.key'),
-      cert: fs.readFileSync('./localhost.crt'),
-    }
+export default defineConfig(({ mode }) => {
+  const isLocal = mode === 'development';
+
+  return {
+    plugins: [
+      vue(),
+    ],
+    base: '/fret-study-buddy/',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '/artyom.window.min.js': fileURLToPath(new URL('./node_modules/artyom.js/build/artyom.window.min.js', import.meta.url))
+      }
+    },
+    server: isLocal ? {
+      https: {
+        key: fs.readFileSync('./localhost.key'),
+        cert: fs.readFileSync('./localhost.crt'),
+      }
+    } : {}
   }
-})
+});
